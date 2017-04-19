fb5aef14878da11faf3f41fe36ac18fb7d7ce676
王甍 <104974660@qq.com>
2017-04-19 22:42:28 +0800

    change deviceName and use chinese name

--- apptest/test.py
+++ apptest/test.py
@@ -4,7 +4,7 @@ from appium import webdriver
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '4.4.2'
-desired_caps['deviceName'] = 'Android Emulator'
+desired_caps['deviceName'] = 'emulator-5554'
 desired_caps['appPackage'] = 'com.android.calculator2'
 desired_caps['appActivity'] = '.Calculator'
 
@@ -16,7 +16,7 @@ driver.find_element_by_name("5").click()
 
 driver.find_element_by_name("9").click()
 
-driver.find_element_by_name("delete").click()
+driver.find_element_by_name("删除").click()
 
 driver.find_element_by_name("9").click()
 
