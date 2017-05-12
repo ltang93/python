bc37f750ad904ccbd37994ca2ec1cbb24e536ec6
ltang93 <104974660@qq.com>
2017-05-12 13:45:09 +0800

    使用PhantomJS，并判断午饭时间

--- python练习/获取上班时间.py
+++ python练习/获取上班时间.py
@@ -1,37 +1,45 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 from python练习 import 时间
 import time
 
-driver=webdriver.Ie()
-mainhandle=driver.current_window_handle  #获得主页句柄
-driver.get('aspireurl')
-driver.find_element_by_link_text('考勤审批').click()
-driver.close()
+driver=webdriver.PhantomJS()
+#mainhandle=driver.current_window_handle  #获得主页句柄
+driver.get('http://eipinfo.aspirecn.com/default.aspx')
+print('已进入：'+driver.title+' ,开始获取...')
+driver.get(driver.find_element_by_link_text('考勤审批').get_attribute('href'))
+#driver.find_element_by_link_text('考勤审批').click()
+#driver.close()
 time.sleep(5)
-for handle in driver.window_handles:
-    driver.switch_to.window(handle)
+# for handle in driver.window_handles:
+#     driver.switch_to.window(handle)
 driver.switch_to.frame('Frame_Content')
 loc=driver.find_element_by_id('ContentPlaceHolder1_btn_UserSearch').send_keys(Keys.ENTER)
-for i in range(30):
-    try:
-        times=driver.find_element_by_id('ContentPlaceHolder1_panel_User')
-        if times.is_displayed():
-            print('Click')
-            break
-    except:
-        pass
-    time.sleep(1)
-    print(str(i))
-    if i==29:
-        print('Time out.')
+
+times=driver.find_element_by_id('ContentPlaceHolder1_panel_User')
+# for i in range(30):
+#     try:
+#         times=driver.find_element_by_id('ContentPlaceHolder1_panel_User')
+#         if times.is_displayed():
+#             print('Click')
+#             break
+#     except:
+#         pass
+#     time.sleep(1)
+#     print(str(i))
+#     if i==29:
+#         print('Time out.')
+#         driver.close()
+#         sys.exit()
 
 
 soup=BeautifulSoup(driver.page_source,"html.parser").find('table',attrs={'id':'ContentPlaceHolder1_GridView_User'}).find_all('td')
 i=0
 day=False
 time=2400
+timelist=[]
 for tds in soup:
     i=i+1
     if(i==2):
@@ -40,14 +48,24 @@ for tds in soup:
         trytime=时间.wtime(tds.get_text())
         if int(trytime) < int(time):
             time = trytime
+        timelist.append(int(trytime))
     if(i==4):
         i=0
         day=False
-#driver.quit()
-print(str(时间.totime(time)))
+driver.close()
+print('上班时间为：'+str(时间.totime(time)))
 
+# timelist=[930,1000,1100,1200,1400,1300,1453]
+timelist.append(1150)
+timelist=sorted(timelist)
 
-
+for i in range(1,5):
+    try:
+        if timelist[timelist.index(1150)+i+1]-timelist[timelist.index(1150)+i]>15:
+            print('当天吃饭时间为 ：'+str(时间.totime(timelist[timelist.index(1150)+i]))+' - '+str(时间.totime(timelist[timelist.index(1150)+i+1])))
+            break
+    except:
+        pass
 '''
 
 
