a3a5fea20eef97e0ec403ca7ca49bece5f00c370
ltang93 <104974660@qq.com>
2017-06-01 17:32:43 +0800

    修复当日期为一位数时，无法判断

--- python练习/时间.py
+++ python练习/时间.py
@@ -1,10 +1,12 @@
-import time
+import time,sys
 
 def wday(datein):
     day=time.localtime(time.time()).tm_mday
-    if(str(datein[-2:])==str(day)):
+    if(str(datein.split('-')[-1])==str(day)):
+        # print('true')
         return True
     else:
+        # print('false')
         return False
 
 def wtime(timein):
@@ -16,10 +18,23 @@ def totime(totimein):
         totimein=str(totimein)[0]+':'+str(totimein)[1:]
     elif(len(str(totimein))==4):
         totimein=str(totimein)[0:2]+':'+str(totimein)[2:]
+    #print(totimein)
     return totimein
 
-#print(time.localtime(time.time()))
-#wday('2017-03-24')
+# print(time.localtime(time.time()))
+# wday('2017-06-22')
 #wtime('9:41')
-#totime(941)
-#totime(1012)
+# totime(941)
+# totime(1012)
+
+# timelist=[930,1000,1100,1200,1400,1300,1453,1210]
+# timelist.append(1150)
+# timelist=sorted(timelist)
+# print(timelist)
+#
+# for i in range(1,5):
+#     if timelist[timelist.index(1150)+i+1]-timelist[timelist.index(1150)+i]>15:
+#         print('当天吃饭时间为 ：'+str(totime(timelist[timelist.index(1150)+i]))+' - '+str(totime(timelist[timelist.index(1150)+i+1])))
+#         break
+
+# print(sys.path[0])
