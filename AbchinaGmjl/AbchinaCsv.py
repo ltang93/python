162f925d8bf508499c3f23fd4fda59d76521f406
wangmeng <104974660@qq.com>
2017-10-30 18:20:40 +0800

    20171030

--- AbchinaGmjl/AbchinaCsv.py
+++ AbchinaGmjl/AbchinaCsv.py
@@ -1,5 +1,6 @@
 import csv,random,sys
 sys.path.append('\\'.join(sys.path[0].split('\\')[:-1]))
+from AbchinaGmjl import AbchinaErrTrain
 
 def CsvLoadTitle(filesrc,num):
     with open(filesrc,'r') as csvfile:
@@ -25,51 +26,72 @@ def CsvLoadAnswer(filesrc,num):
             if(row['序号']==str(num)):
                 return row['答案']
 
-if __name__ == '__main__':
-    while(1):
-        filenum=random.randint(1,3)
-        if filenum==1:
-            filename='2017-单选.csv'
-            num=random.randint(1,807)
-        elif filenum==2:
-            filename = '2017-多选.csv'
-            num = random.randint(1, 599)
+def guess(filenamelist,numlist):
+    while (1):
+        filenum = random.randint(1, 3)
+        if filenum == 1:
+            # filename = '2017-单选.csv'
+            filename=filenamelist[0]
+            # num = random.randint(1, 807)
+            num=random.randint(1,numlist[0])
+        elif filenum == 2:
+            # filename = '2017-多选.csv'
+            filename=filenamelist[1]
+            # num = random.randint(1, 599)
+            num=random.randint(1,numlist[1])
         else:
-            filename = '2017-判断.csv'
-            num = random.randint(1, 598)
+            # filename = '2017-判断.csv'
+            filename=filenamelist[2]
+            # num = random.randint(1, 598)
+            num=random.randint(1,numlist[2])
 
-        title=CsvLoadTitle(filename,num)
-        if filenum==1 or filenum==2:
-            if filenum==1:
-                title='(单选)'+title
-            elif filenum==2:
+        title = CsvLoadTitle(filename, num)
+        if filenum == 1 or filenum == 2:
+            if filenum == 1:
+                title = '(单选)' + title
+            elif filenum == 2:
                 title = '(多选)' + title
-            options=CsvLoadOption(filename,num)
+            options = CsvLoadOption(filename, num)
             i = 0
             for option in options:
-                option=str(option).replace(' ','')
+                option = str(option).replace(' ', '')
                 title = title + '\n' + chr(65 + i) + '. ' + option
-                i+=1
+                i += 1
         else:
-            title='(判断题,1=对，0=错)'+title
+            title = '(判断题,1=对，0=错)' + title
         print(title)
         print('请输入你的回答')
-        useranswer=input()
-        answer=CsvLoadAnswer(filename,num)
+        useranswer = input()
+        answer = CsvLoadAnswer(filename, num)
+        with open('finish.csv', 'a') as finishfile:
+            finishfile.write(str(filenum) + ',' + str(num))
+            finishfile.write('\n')
         if useranswer.upper() == answer.upper():
             print('回答正确')
-        elif useranswer=='1' and answer == '对':
+            with open('right.csv', 'a') as rightfile:
+                rightfile.write(str(filenum) + ',' + str(num))
+                rightfile.write('\n')
+        elif useranswer == '1' and answer == '对':
             print('回答正确')
-        elif useranswer=='0' and answer == '错':
+            with open('right.csv', 'a') as errorfile:
+                errorfile.write(str(filenum) + ',' + str(num))
+                errorfile.write('\n')
+        elif useranswer == '0' and answer == '错':
             print('回答正确')
+            with open('right.csv', 'a') as errorfile:
+                errorfile.write(str(filenum) + ',' + str(num))
+                errorfile.write('\n')
         else:
             print('回答错误')
-            print('正确答案:'+str(answer))
-            with open('error.csv','a') as errorfile:
-                errorfile.write(str(filenum)+','+str(num))
+            print('正确答案:' + str(answer))
+            with open('error.csv', 'a') as errorfile:
+                errorfile.write(str(filenum) + ',' + str(num))
                 errorfile.write('\n')
         print('\n')
 
+
+if __name__ == '__main__':
+    guess(filenamelist=['2017-单选.csv','2017-多选.csv','2017-判断.csv'],numlist=[807,599,598])
 '''
     def __init__(self):
         self.filename = '2017-单选.csv'
--- AbchinaGmjl/AbchinaErrTrain.py
+++ AbchinaGmjl/AbchinaErrTrain.py
@@ -1,19 +1,36 @@
-import csv,random,sys
+import csv,random,sys,os
 sys.path.append('\\'.join(sys.path[0].split('\\')[:-1]))
 from  AbchinaGmjl import AbchinaCsv
 
-try:
-    with open('error.csv', 'r') as errorfile:
-        s1, s2, s3 = set([]), set([]), set([])
-        for line in errorfile:
-            if line.split(',')[0] == '1':
-                s1.add(int(line.split(',')[1]))
-            elif line.split(',')[0] == '2':
-                s2.add(int(line.split(',')[1]))
-            elif line.split(',')[0] == '3':
-                s3.add(int(line.split(',')[1]))
-except:
-    print('操作错误，没有错题文件，请先进行练习！')
+def sortcsv():
+    try:
+        if os.path.exists('error.csv'):
+            with open('error.csv', 'r') as errorfile:
+                err1, err2, err3 = set([]), set([]), set([])
+                for line in errorfile:
+                    if line.split(',')[0] == '1':
+                        err1.add(int(line.split(',')[1]))
+                    elif line.split(',')[0] == '2':
+                        err2.add(int(line.split(',')[1]))
+                    elif line.split(',')[0] == '3':
+                        err3.add(int(line.split(',')[1]))
+                print(err1,err2,err3)
+        if os.path.exists('finish.csv'):
+            with open('finish.csv', 'r') as finishfile:
+                fi1, fi2, fi3 = set([]), set([]), set([])
+                for line in finishfile:
+                    if line.split(',')[0] == '1':
+                        fi1.add(int(line.split(',')[1]))
+                    elif line.split(',')[0] == '2':
+                        fi2.add(int(line.split(',')[1]))
+                    elif line.split(',')[0] == '3':
+                        fi3.add(int(line.split(',')[1]))
+                print(fi1,fi2,fi3)
+    except:
+        pass
+        # print('操作错误，请先进行练习！')
 
-for i in range(1,3):
-    pass # do what ??
+    for i in range(1,3):
+        if i ==1:
+            for
+
